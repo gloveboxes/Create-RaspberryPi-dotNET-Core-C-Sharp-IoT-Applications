41addfb6ac1f36be4e48fdae54dff9bb1e39f6ad
Dave Glover <dglover@microsoft.com>
2020-09-01 15:53:27 +1000

    Plug and Play V2 samples added

---
+++ samples/dotnet.core.iot.csharp.iothub.device-twins.sense.hat/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Iot.Device.CpuTemperature;
+using Iot.Device.SenseHat;
+using Iot.Device.Common;
+using Newtonsoft.Json;
+
+// Sample based on Azure IoT C# SDK Samples. 
+// https://github.com/Azure-Samples/azure-iot-samples-csharp
+
+namespace dotnet.core.iot
+{
+    class Program
+    {
+        const string DeviceConnectionString = "<Your Azure IoT Hub Connection String>";
+
+        // https://github.com/Azure/opendigitaltwins-dtdl
+        // https://github.com/Azure/opendigitaltwins-dtdl/blob/master/DTDL/v2/dtdlv2.md#telemetry
+
+        const string plugAndPlayModelId = "dtmi:Glovebox:Hvac;1";
+        static ClientOptions options = new ClientOptions() { ModelId = plugAndPlayModelId };
+        static DeviceClient iotClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt, options);
+        static CpuTemperature _temperature = new CpuTemperature();
+        static SenseHatLedMatrixI2c ledMatrix = new SenseHatLedMatrixI2c();
+        static SenseHatTemperatureAndHumidity th = new SenseHatTemperatureAndHumidity();
+        static int _msgId = 0;
+        static int thermostat = 0;
+        enum RoomAction { Unknown, Heating, Cooling, Green }
+        static RoomAction previousRoomState = RoomAction.Unknown;
+        static int previousRoomTemperature = 0;
+        static RoomAction roomState = RoomAction.Unknown;
+
+        static async Task Main(string[] args)
+        {
+            double temperature, humidity, pressure;
+            var rand = new Random();
+
+            await iotClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChangedAsync, null).ConfigureAwait(false);   // callback for Device Twin updates
+            await DeviceTwinGetInitialState(iotClient); // Get current cloud state of the device twin
+
+            while (true)
+            {
+                temperature = Math.Round(th.Temperature.DegreesCelsius, 1);
+                humidity = Math.Round(th.Humidity, 1);
+                pressure = 1000 + rand.Next(0, 200);
+
+                try
+                {
+                    Console.WriteLine($"Ambient temperature {temperature} and humidity {humidity}");
+                    await SendMsgIotHub(temperature, humidity, pressure);
+
+                    roomState = (int)temperature > thermostat ? RoomAction.Cooling : (int)temperature < thermostat ? RoomAction.Heating : RoomAction.Green;
+                    await UpdateRoomAction(roomState);
+                    await UpdateRoomTemperature(temperature);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("exception msg: " + ex.Message);
+                }
+
+                Thread.Sleep(4000); // sleep for 4 seconds
+            }
+        }
+
+        private static async Task SendMsgIotHub(double temperature, double humidity, double pressure)
+        {
+            var telemetry = new Telemetry() { Temperature = temperature, Humidity = humidity, Pressure=pressure, MessageId = _msgId++ };
+            string json = JsonConvert.SerializeObject(telemetry);
+
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(json));
+
+            await iotClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+        }
+
+        private static async Task DeviceTwinGetInitialState(DeviceClient iotClient)
+        {
+            Twin twin = await iotClient.GetTwinAsync().ConfigureAwait(false);
+            if (twin.Properties.Desired.Contains("Thermostat"))
+            {
+                int.TryParse(Convert.ToString(twin.Properties.Desired["Thermostat"]["value"]), out thermostat);
+            }
+        }
+
+        private static async Task OnDesiredPropertyChangedAsync(TwinCollection desiredProperties, object userContext)
+        {
+            if (desiredProperties.Contains("Thermostat"))
+            {
+                if (int.TryParse(Convert.ToString(desiredProperties["Thermostat"]["value"]), out thermostat))
+                {
+                    await UpdateDeviceTwin("ThermostatSetting", thermostat);
+                }
+            }
+        }
+
+        private static async Task UpdateRoomTemperature(double temperature)
+        {
+            if (previousRoomTemperature != (int)temperature)
+            {
+                await UpdateDeviceTwin("RoomTemperature", temperature);
+                previousRoomTemperature = (int)temperature;
+            }
+        }
+
+        private static async Task UpdateRoomAction(RoomAction roomState)
+        {
+            if (roomState != previousRoomState)
+            {
+                await UpdateDeviceTwin("RoomAction", roomState.ToString());
+                previousRoomState = roomState;
+
+                switch (roomState)
+                {
+                    case RoomAction.Cooling:
+                        ledMatrix.Fill(Color.Blue);
+                        break;
+                    case RoomAction.Heating:
+                        ledMatrix.Fill(Color.Red);
+                        break;
+                    default:
+                        ledMatrix.Fill(Color.Green);
+                        break;
+                }
+            }
+        }
+
+        private static async Task UpdateDeviceTwin(string propertyName, object value)
+        {
+            TwinCollection reportedProperties = new TwinCollection();
+            reportedProperties[propertyName] = value;
+            await iotClient.UpdateReportedPropertiesAsync(reportedProperties).ConfigureAwait(false);
+        }
+
+        class Telemetry
+        {
+            [JsonPropertyAttribute(PropertyName = "Temperature")]
+            public double Temperature { get; set; } = 0;
+
+            [JsonPropertyAttribute(PropertyName = "Humidity")]
+            public double Humidity { get; set; } = 0;
+
+            [JsonPropertyAttribute(PropertyName = "Pressure")]
+            public double Pressure { get; set; } = 0;
+
+            [JsonPropertyAttribute(PropertyName = "MsgId")]
+            public int MessageId { get; set; } = 0;
+        }
+    }
+}
--- samples/dotnet.core.iot.csharp.iothub/Program.cs
+++ samples/dotnet.core.iot.csharp.iothub/Program.cs
@@ -12,46 +12,62 @@ namespace dotnet.core.iot
     {
         const string DeviceConnectionString = "<Your Azure IoT Hub Connection String>";
 
-        // Replace with the device id you used when you created the device in Azure IoT Hub
-        const string DeviceId = "<Your Device Id>";
-        static DeviceClient _deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt);
+        // https://github.com/Azure/opendigitaltwins-dtdl
+        // https://github.com/Azure/opendigitaltwins-dtdl/blob/master/DTDL/v2/dtdlv2.md#telemetry
+
+
+        const string plugAndPlayModelId = "dtmi:Glovebox:Hvac;1";
+        static ClientOptions options = new ClientOptions() { ModelId = plugAndPlayModelId };
+        static DeviceClient _deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt, options);
         static CpuTemperature _temperature = new CpuTemperature();
         static int _msgId = 0;
         const double TemperatureThreshold = 42.0;
 
         static async Task Main(string[] args)
         {
+            double temperature, humidity, pressure;
+            var rand = new Random();
+
             while (true)
             {
-                if (_temperature.IsAvailable)
-                {
-                    Console.WriteLine($"The CPU temperature is {Math.Round(_temperature.Temperature.Celsius, 2)}");
-                    await SendMsgIotHub(_temperature.Temperature.Celsius);
-                }
-                Thread.Sleep(2000); // sleep for 2000 milliseconds
+                if (_temperature.IsAvailable) { temperature = Math.Round(_temperature.Temperature.DegreesCelsius, 2); }
+                else { temperature = rand.Next(15, 35); }
+
+                humidity = 40 + rand.Next(0, 60);
+                pressure = 1000 + rand.Next(0, 200);
+
+                Console.WriteLine($"The CPU temperature is {temperature}");
+
+                await SendMsgIotHub(temperature, humidity, pressure);
+
+                Thread.Sleep(4000); // sleep for 4 seconds
             }
         }
 
-        private static async Task SendMsgIotHub(double temperature)
+        private static async Task SendMsgIotHub(double temperature, double humidity, double pressure)
         {
-            var telemetry = new Telemetry() { Temperature = Math.Round(temperature, 2), MessageId = _msgId++ };
+            var telemetry = new Telemetry() { Temperature = temperature, Humidity = humidity, Pressure = pressure, MessageId = _msgId++ };
             string json = JsonConvert.SerializeObject(telemetry);
 
             Message eventMessage = new Message(Encoding.UTF8.GetBytes(json));
             eventMessage.Properties.Add("temperatureAlert", (temperature > TemperatureThreshold) ? "true" : "false");
+
             await _deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
         }
 
         class Telemetry
         {
-            [JsonPropertyAttribute (PropertyName="temperature")] 
+            [JsonPropertyAttribute(PropertyName = "Temperature")]
             public double Temperature { get; set; } = 0;
 
-            [JsonPropertyAttribute (PropertyName="messageId")] 
-            public int MessageId { get; set; } = 0;
+            [JsonPropertyAttribute(PropertyName = "Humidity")]
+            public double Humidity { get; set; } = 0;
 
-            [JsonPropertyAttribute (PropertyName="deviceId")] 
-            public string DeviceId {get; set;} = Program.DeviceId;
+            [JsonPropertyAttribute(PropertyName = "Pressure")]
+            public double Pressure { get; set; } = 0;
+
+            [JsonPropertyAttribute(PropertyName = "MsgId")]
+            public int MessageId { get; set; } = 0;
         }
     }
 }
